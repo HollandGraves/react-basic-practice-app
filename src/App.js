c87491970873bb6de86c73a6cc25e9205b73085f
Holland Graves <zacharyledoux13@gmail.com>
2018-09-10 12:19:25 -0600

    added some more comments to code to understand better what is going on. Also, added the onClick functionality that uses a reducer to change the state, and passed that function down to props into a statless component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Person from './Components/Person/Person'
 
 class App extends Component {
+  // state is all the data of the app that can be dynamic
   state = {
     persons: [
         { name: 'Matt', gender: 'male' },
@@ -12,15 +13,15 @@ class App extends Component {
     ]
   }
 
-  switchNameHandler = () => {
-    // console.log('Was Clicked!');
+  switchNameHandler = (newName) => {
+    // this is the method built into React that changes the state and causes the component to refresh and update
     this.setState(
       {
         persons: [
-          { name: 'Matthew Matias', gender: 'male' },
-          { name: 'Legolas the Archer', gender: 'male' },
-          { name: 'Dustin Higginbotham', gender: 'male' },
-          { name: 'Katherine Higginbotham', gender: 'female' }
+          { name: newName, gender: 'male' },
+          { name: newName, gender: 'male' },
+          { name: newName, gender: 'male' },
+          { name: newName, gender: 'female' }
         ]
       }
     )
@@ -30,11 +31,30 @@ class App extends Component {
     return (
       <div className="App">
         <h1>Yo yo yo yo embryooo</h1>
-        <button onClick={this.switchNameHandler}>Switch Name</button>
-        <Person name={this.state.persons[0].name} gender={this.state.persons[0].gender}/>
-        <Person name={this.state.persons[1].name} gender={this.state.persons[1].gender}/>
-        <Person name={this.state.persons[2].name} gender={this.state.persons[2].gender}/>
-        <Person name={this.state.persons[3].name} gender={this.state.persons[3].gender}>
+        {// this type of event below (onClick, a reserved keyword) calls the Reducer function (aka handler) and changes the state with this.setState and the argument passed
+        }
+        <button onClick={this.switchNameHandler.bind(this, 'New Names HAVE BEEN INSERTED')}>     
+          Switch Name
+        </button>
+        <Person 
+          name={this.state.persons[0].name}         // this type of passing data hardcodes data into the component through props, and is accessed through, in this case, e.g. props.name
+          gender={this.state.persons[0].gender}
+        />
+        <Person 
+          click={() => this.switchNameHandler('Leg the LAS, the FaRTchurrdr')}          // this line passes the reducer function (handler) into the props of the Person component
+          name={this.state.persons[1].name} 
+          gender={this.state.persons[1].gender}
+        />
+        <Person 
+          click={() => this.switchNameHandler('Leg the LAS, the FaRTchurrdr')} 
+          name={this.state.persons[2].name} 
+          gender={this.state.persons[2].gender}
+        />
+        <Person 
+          click={() => this.switchNameHandler('Leg the LAS, the FaRTchurrdr')} 
+          name={this.state.persons[3].name} 
+          gender={this.state.persons[3].gender}       // below, inbetween the <Person></Person> tag is hardcoded information that is passed into the component, and acessed through props.children, within the component
+        > 
           This is the text that is passed in through props.children or this.props.children
           <br/>
           (this props used depends on if the JSX-html tag comes from a stateless Component or a stateful Component, respectively)
--- src/Components/Person/Person.js
+++ src/Components/Person/Person.js
@@ -4,10 +4,13 @@ import React from 'react'
 // This is a stateless component, or a component that exports a handbuilt function, versus a class, and does not change state
 const person = (props) => {
     return (
+        // The onClick function calls the click prop this contains a reducer that changes state, all this is passed down from the root component
         <div>
-            <p> {props.name} is awesome!</p>
+            <p onClick={props.click}>  
+                {props.name} is awesome!
+            </p>
             <p>and is the gender: {props.gender}</p>
-            <p>{props.children}</p>
+            <p>{props.children}</p> 
             <br />
         </div>
     );
