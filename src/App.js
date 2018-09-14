230f8d6faf9f7017ab25b62c1ed064f3990c8427
Holland Graves <zacharyledoux13@gmail.com>
2018-09-13 20:46:22 -0600

    added some more with two way binding, and inline styling

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 import Person from './Components/Person/Person'
 
+
 class App extends Component {
   // state is all the data of the app that can be dynamic
   state = {
@@ -27,33 +28,95 @@ class App extends Component {
     )
   }
 
+  switchTypedHandler = (event) => {
+    this.setState(
+      {
+        persons: [
+            { name: event.target.value, gender: 'male' },
+            { name: 'Legolas', gender: 'male' },
+            { name: 'Dustin', gender: 'male' },
+            { name: 'Kat', gender: 'female' }
+        ]
+      }
+    )
+  }
+
+  resetNameHandler = () => {
+    this.setState(
+      {
+        persons: [
+            { name: 'Matt', gender: 'male' },
+            { name: 'Legolas', gender: 'male' },
+            { name: 'Dustin', gender: 'male' },
+            { name: 'Kat', gender: 'female' }
+        ]
+      }
+    )
+  }
+
+
+
+
   render() {
+
+
+
+    
+
+    const style = {                   // this is an example of how to write inline styles. It uses jS camelCase notation to address the style object properties to an actual style
+      backgroundColor: 'white',
+      font: 'inherit',
+      border: '1px solid blue',
+      padding: '8px',
+      cursor: 'pointer'
+    }
+
+
+
+
+
     return (
       <div className="App">
         <h1>Yo yo yo yo embryooo</h1>
         {// this type of event below (onClick, a reserved keyword) calls the Reducer function (aka handler) and changes the state with this.setState and the argument passed
         }
-        <button onClick={this.switchNameHandler.bind(this, 'New Names HAVE BEEN INSERTED')}>     
+
+        <button 
+            className="appButton"
+            style={style}           
+            onClick={this.switchNameHandler.bind(this, 'The names have been changed with the click of a button!')}      // because of the text being bound to the method switchNameHandler the text will be passed into the method
+            >           
           Switch Name
         </button>
+
+        <button
+            className="appButton"
+            style={style}
+            onClick={() => this.resetNameHandler()}>
+          Reset Name
+        </button>
+
         <Person 
           name={this.state.persons[0].name}         // this type of passing data hardcodes data into the component through props, and is accessed through, in this case, e.g. props.name
           gender={this.state.persons[0].gender}
+          click={() => this.switchNameHandler('The names have been changed with the click of an element!')}          // this line passes the function into the props of the Person component
+          changed={this.switchTypedHandler}         // this will pass the second event handler function into props so, inevitabely, it will change the name based up the name typed into the input bar in the Person component
         />
         <Person 
-          click={() => this.switchNameHandler('Leg the LAS, the FaRTchurrdr')}          // this line passes the reducer function (handler) into the props of the Person component
           name={this.state.persons[1].name} 
           gender={this.state.persons[1].gender}
+          click={() => this.switchNameHandler('The names have been changed with the click of an element!')}
         />
         <Person 
-          click={() => this.switchNameHandler('Leg the LAS, the FaRTchurrdr')} 
           name={this.state.persons[2].name} 
           gender={this.state.persons[2].gender}
+          click={() => this.switchNameHandler('The names have been changed with the click of an element!')}
         />
         <Person 
-          click={() => this.switchNameHandler('Leg the LAS, the FaRTchurrdr')} 
           name={this.state.persons[3].name} 
-          gender={this.state.persons[3].gender}       // below, inbetween the <Person></Person> tag is hardcoded information that is passed into the component, and acessed through props.children, within the component
+          gender={this.state.persons[3].gender}       
+          click={() => this.switchNameHandler('The names have been changed with the click of an element!')}
+          // below, inbetween the <Person></Person> tag is hardcoded information that is passed into the component, and acessed through props.children, within the component
         > 
           This is the text that is passed in through props.children or this.props.children
           <br/>
@@ -61,6 +124,7 @@ class App extends Component {
           <br/>
           note: you can put html in this space that is passed into the props.children or this.props.children javascript call
         </Person>
+
       </div>
     );
     // this, directly below, is what the JSX compiles to
--- src/Components/Person/Person.js
+++ src/Components/Person/Person.js
@@ -1,17 +1,23 @@
 import React from 'react'
+import Person from './Person.css'
+
 
 
 // This is a stateless component, or a component that exports a handbuilt function, versus a class, and does not change state
 const person = (props) => {
     return (
         // The onClick function calls the click prop this contains a reducer that changes state, all this is passed down from the root component
-        <div>
-            <p onClick={props.click}>  
-                {props.name} is awesome!
+        <div className='Person'>
+            <p>  
+                {props.name}
+            </p>
+            <p className="pChange" onClick={props.click}>
+                Click me to change the names of everyone!
             </p>
             <p>and is the gender: {props.gender}</p>
             <p>{props.children}</p> 
             <br />
+            <input type="text" onChange={props.changed} value={props.name} />
         </div>
     );
 }
